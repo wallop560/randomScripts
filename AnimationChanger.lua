7267abae43f7bba65768c7c9a6c5baad3527a0a3
wallop560 <72463875+wallop560@users.noreply.github.com>
2022-08-23 21:28:37 +0100

    Create AnimationChanger.lua

---
+++ AnimationChanger.lua
@@ -0,0 +1,67 @@
+local Players = game:GetService('Players')
+local HttpService = game:GetService('HttpService')
+local MarketplaceService = game:GetService("MarketplaceService")
+
+
+
+local LocalPlayer = Players.LocalPlayer
+
+local Bundles = HttpService:JSONDecode(game:HttpGet('https://pastebin.com/raw/XppaAPF7'))
+
+function ApplyAnimations(Animations,Character)
+    local Animate = Character:FindFirstChild('Animate') or Character:WaitForChild('Animate')
+    
+    for Animation,Ids in next,Animations do
+        local AnimObject = Animate:WaitForChild(Animation,4)
+        for i,id in next,Ids do
+            local AnimationObject
+
+            if Animation == 'idle' then
+                AnimationObject = AnimObject['Animation'..tostring(i)]
+            elseif Animation == 'swim' and i == 2 then 
+                AnimationObject = Animate:WaitForChild('swimidle',1):WaitForChild('SwimIdle',1) 
+            elseif Animation == 'swim' then
+                local firststchar = string.upper(string.sub(Animation,1,1))
+                local restchar = string.sub(Animation,2,#Animation)
+                local fullstr = firststchar..restchar
+
+                AnimationObject = AnimObject[fullstr]
+            else
+                local firststchar = string.upper(string.sub(Animation,1,1))
+                local restchar = string.sub(Animation,2,#Animation)
+                local fullstr = firststchar..restchar
+
+                AnimationObject = AnimObject[fullstr..'Anim']
+            end
+            print(tostring(AnimationObject))
+            AnimationObject.AnimationId = tostring(id)
+        end
+    end
+end
+
+function UpdateCharacter(Character)
+    local Emotes = getgenv().emotes 
+    local equipedEmotes = {}
+    local AnimationPack = getgenv().animationPack
+    local Animations = getgenv().animations
+    local Humanoid = Character:FindFirstChildOfClass('Humanoid') or Character:WaitForChild('Humanoid')
+    for _,v in next,Emotes do 
+        if typeof(v) ~= 'table' then
+            Emotes[_] = {v}
+        end
+        table.insert(equipedEmotes,tostring(_))
+    end
+    Humanoid.HumanoidDescription:SetEmotes(Emotes)
+    Humanoid.HumanoidDescription:SetEquippedEmotes(equipedEmotes)
+    if AnimationPack and Bundles[AnimationPack] then
+        print('bundle')
+        ApplyAnimations(Bundles[AnimationPack],Character)
+    else
+        print('individual')
+        ApplyAnimations(Animations,Character)
+    end
+end
+
+--UpdateCharacter(LocalPlayer.Character)
+pcall(UpdateCharacter,LocalPlayer.Character)
+LocalPlayer.CharacterAdded:Connect(UpdateCharacter)
